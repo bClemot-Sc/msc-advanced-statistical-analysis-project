efdd129af90911fe7b5872ce5059ff0758f75351
paquittet <pa.quittet@gmail.com>
2023-10-02 15:49:01 +0200

    Bilan réunion 02/10
    - Renommage des variables
    - Suppression des variables corrélées (colinéarité)
    - Identification des variables catégorielles à relevel (sun_flight et wind_flight)

--- ASA_PROJET_SCRIPT.R
+++ ASA_PROJET_SCRIPT.R
@@ -1,6 +1,8 @@
 rm(list=ls())  # Efface les variables créées lors des exécutions précédentes
 graphics.off() # Ferme les fenêtres ouvertes lors des exécutions précédentes
 
+# Packages
+library(corrplot)
 
 # Define WD
 setwd("C:/Users/p_a_8/OneDrive/Bureau/M2 MODE/COURS/Semestre 3/ASA/PROJET")
@@ -19,18 +21,45 @@ str(data)
 
 
 
+# Rename colnames ---------------------------------------------------------
+colnames(data)[1:22] <- c(
+  "ID",
+  "eclosion_day",
+  "sex",
+  "FMR_day",
+  "age_FMR",
+  "mass",
+  "FMR",
+  "flight_day",
+  "age_flight",
+  "sun_flight",
+  "wind_flight",
+  "air_T",
+  "dew_flight",
+  "humidity_flight",
+  "solar_radiation_flight",
+  "flight_duration",
+  "disturbed_flight",
+  "stop_flight",
+  "takeoff_time",
+  "takeoff_T_thorax",
+  "end_flight_time",
+  "y"
+)
+
 # Data cleaning -----------------------------------------------------------
 
 # On supprime la colonne "ID" inutile
-data <- data[, !colnames(data) %in% "Butterfly_ID"]
+data <- data[, !colnames(data) %in% "ID"]
 
 # Conversion des variables "heures"
-data$Flight_takeoff_time_flight1 <-  format(as.POSIXct(data$Flight_takeoff_time_flight1, format = "%H:%M:%S"), format = "%H:%M:%S")
-data$flight_end_time_flight1 <-  format(as.POSIXct(data$flight_end_time_flight1, format = "%H:%M:%S"), format = "%H:%M:%S")
+data$takeoff_time <-  format(as.POSIXct(data$takeoff_time, format = "%H:%M:%S"), format = "%H:%M:%S")
+data$end_flight_time <-  format(as.POSIXct(data$end_flight_time, format = "%H:%M:%S"), format = "%H:%M:%S")
 
 # Conversion des variables "facteurs"
-data$Wind_flight1 <- as.factor(data$Wind_flight1)
-data$Sun_flight1 <- as.factor(data$Sun_flight1)
+data$wind_flight <- as.factor(data$wind_flight)
+data$sun_flight <- as.factor(data$sun_flight)
+
 
 
 # Identification des données manquantes (en %)
@@ -47,10 +76,10 @@ data <- data[, !colnames(data) %in% names(data_na[data_na>30])]
 # Data visualisation (distribution & outliers) ------------------------------------------------------
 # --- Visualisation de la variable Y
 par(mfrow = c(1,3))
-hist(data$Flight_Tb_average_C_flight1, pch = 16, col = 'blue', xlab = "Mean temperature", main = "")
-dotchart(data$Flight_Tb_average_C_flight1, pch = 16, col = 'blue')
-qqnorm(data$Flight_Tb_average_C_flight1, pch = 16, col = 'blue')
-qqline(data$Flight_Tb_average_C_flight1, col = 'red')
+hist(data$y, pch = 16, col = 'blue', xlab = "Mean temperature", main = "")
+dotchart(data$y, pch = 16, col = 'blue')
+qqnorm(data$y, pch = 16, col = 'blue')
+qqline(data$y, col = 'red')
 
 # ---- Visualisation des distribution des X
 # distribution des facteurs
@@ -60,9 +89,9 @@ par(mfrow = c(2,4))
 i <- 1
 apply(X = data_factor, MARGIN = 2, function(x){
   
-  bar <- barplot(table(x), main = colnames(data_factor)[i])
+  bar <- barplot(table(x), main = colnames(data_factor)[i], cex.main = .8)
   bar
-  text(x = bar, y = table(x) - .2, label = table(x), pos = 3)  # Effectifs par catégorie
+  text(x = bar, y = table(x) - 1, label = table(x), pos = 3)  # Effectifs par catégorie
   
   i <<- i + 1    
 }
@@ -92,13 +121,13 @@ i <- 1
 apply(X = data_factor, MARGIN = 2, function(x){
   
   boxplot(
-    data$Flight_Tb_average_C_flight1 ~ x,
+    data$y ~ x,
     ylab = "Y",
     xlab = "",
     main = colnames(data_factor)[i], 
     ylim = c(
-      min(data$Flight_Tb_average_C_flight1, na.rm = T) - 1,
-      max(data$Flight_Tb_average_C_flight1  + 1, na.rm = T)
+      min(data$y, na.rm = T) - 1,
+      max(data$y  + 1, na.rm = T)
     )
   )
   i <<- i + 1    
@@ -113,16 +142,16 @@ i <- 1
 apply(X = data_num, MARGIN = 2, function(x){
   
   plot(
-    data$Flight_Tb_average_C_flight1  ~ x,
+    data$y  ~ x,
     ylab = "T",
     xlab = "",
     main = colnames(data_num)[i],
     ylim = c(
-      min(data$Flight_Tb_average_C_flight1, na.rm = T) - 1,
-      max(data$Flight_Tb_average_C_flight1  + 1, na.rm = T)
+      min(data$y, na.rm = T) - 1,
+      max(data$y  + 1, na.rm = T)
     )
   )
-  abline(lm(data$Flight_Tb_average_C_flight1  ~ x), col = "red")
+  abline(lm(data$y  ~ x), col = "red")
   
   i <<- i + 1    
 }
@@ -134,12 +163,26 @@ apply(X = data_num, MARGIN = 2, function(x){
 # Interactions between Latitude & Location
 par(mfrow = c(1,1))
 plot(
-  data$Flight_Tb_average_C_flight1 ~ data$Air_temperature_C_flight1,
+  data$y ~ data$air_T,
   ylim = c(
-    min(data$Flight_Tb_average_C_flight1, na.rm = T) - 1,
-    max(data$Flight_Tb_average_C_flight1  + 1, na.rm = T)
+    min(data$y, na.rm = T) - 1,
+    max(data$y  + 1, na.rm = T)
   )
 )
-points(data$Flight_Tb_average_C_flight1[data$Pgi_331 == "AA"] ~ data$Air_temperature_C_flight1[data$Pgi_331 == "AA"], pch = 20, cex = 2, col = "red")
-points(data$Flight_Tb_average_C_flight1[data$Pgi_331 == "CA"] ~ data$Air_temperature_C_flight1[data$Pgi_331 == "CA"], pch = 20, cex = 2, col = "blue")
-points(data$Flight_Tb_average_C_flight1[data$Pgi_331 == "CC"] ~ data$Air_temperature_C_flight1[data$Pgi_331 == "CC"], pch = 20, cex = 2, col = "green")
+points(data$y[data$Pgi_331 == "AA"] ~ data$air_T[data$Pgi_331 == "AA"], pch = 20, cex = 2, col = "red")
+points(data$y[data$Pgi_331 == "CA"] ~ data$air_T[data$Pgi_331 == "CA"], pch = 20, cex = 2, col = "blue")
+points(data$y[data$Pgi_331 == "CC"] ~ data$air_T[data$Pgi_331 == "CC"], pch = 20, cex = 2, col = "green")
+
+
+
+
+# Colinéarité des variables x -------------------------------------------------------------
+M <- cor(na.omit(data_num))
+corrplot.mixed(M,upper="square",lower.col="black", tl.col="black",cl.cex = 0.8,tl.cex = 0.7,number.cex =0.8)
+
+# Corrélation des variables temporelles
+# cor(na.omit(data$takeoff_time ~ data$end_flight_time))
+
+# --- Suppression des variables corrélées (dew point ou humidity ?)
+data <- data[!colnames(data) %in% c("eclosion_day", "flight_day", "age_flight", "dew_flight", "stop_flight")]
+
